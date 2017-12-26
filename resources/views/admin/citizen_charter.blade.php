f93f5282af15f6b844094049fb3d0d872b5b9361
sean <aseanroger30@gmail.com>
2017-12-26 09:47:35 +0800

    Added missing tabs on citizen charters page

--- resources/views/admin/citizen_charter.blade.php
+++ resources/views/admin/citizen_charter.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
 <div>
-    <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation"><a href="#vision_mission" aria-controls="vision_mission" data-toggle="tab">Vision Mission</a></li>
-        <li class="active" role="presentation"><a href="#organizaions_functions" aria-controls="organizations_functions" data-toggle="tab">Organizations & Functions</a></li>
+    <ul id="citizencharter" class="nav nav-tabs" role="tablist">
+        <li class="active" role="presentation"><a href="#vision_mission" aria-controls="vision_mission" data-toggle="tab">Vision Mission</a></li>
+        <li role="presentation"><a href="#orgfunc" aria-controls="organizations_functions" data-toggle="tab">Organizations & Functions</a></li>
+        <li role="presentation"><a href="#service" aria-controls="organizations_functions" data-toggle="tab">Service Pledge</a></li>
+        <li role="presentation"><a href="#feedback" aria-controls="organizations_functions" data-toggle="tab">Receiving Feedback</a></li>
+        <li role="presentation"><a href="#frontline" aria-controls="organizations_functions" data-toggle="tab">Frontline Service</a></li>
+        <li role="presentation"><a href="#officials" aria-controls="organizations_functions" data-toggle="tab">DSWD Officials and Key Positions</a></li>
     </ul>
     <div class="tab-content">
-        <div class="tab-pane" id="vision_mission" role="tabpanel">@include('admin.vision_mission')</div>
-        <div class="tab-pane active" id="organizations_functions" role="tabpanel">@include('admin.organizations_functions')</div>
+        <div class="tab-pane active" id="vision_mission" role="tabpanel">@include('admin.vision_mission')</div>
+        <div class="tab-pane" role="tabpanel" id="orgfunc">@include('admin.organizations_functions')</div>
+        <div class="tab-pane" role="tabpanel" id="service">@include('admin.service_pledge')</div>
+        <div class="tab-pane" role="tabpanel" id="feedback">@include('admin.receiving_feedback')</div>
+        <div class="tab-pane" role="tabpanel" id="frontline">@include('admin.frontline_service')</div>
+        <div class="tab-pane" role="tabpanel" id="officials">@include('admin.dswd_officials')</div>
     </div>
 </div>
 @endsection
---
+++ resources/views/admin/dswd_officials.blade.php
@@ -0,0 +1,7 @@
+<div class="col-md-12">
+    <h1>DSWD OFFICIALS & KEY POSITIONS</h1>
+    <br>
+    <div>
+        <p>Ipsum voluptatum ad nobis suscipit magnam ut at porro quae odit voluptas. Possimus praesentium odio corrupti adipisci officia consequatur quisquam laudantium, molestias quaerat deserunt consequuntur. Facere quasi pariatur perferendis sint porro illo neque. Eveniet dolor amet rem officiis perspiciatis sunt quae consequatur, tempore quaerat molestiae. Nostrum cumque temporibus non vel tempore nihil! Neque accusamus tempora explicabo adipisci tenetur beatae non architecto. Sit id atque molestias possimus reiciendis odit vel eum fugit vitae voluptatibus inventore. Labore vero eius explicabo deserunt quidem veniam necessitatibus. Quia fugiat ducimus sint incidunt atque a. Nostrum nam soluta amet blanditiis eaque. Incidunt vel error aliquid earum.</p>
+    </div>
+</div>
