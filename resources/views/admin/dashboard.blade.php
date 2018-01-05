a097622bfdeebe4aeaeabe5f8c94758b3211e981
Sally Jean Sumeguin <sally.sumeguin@intechsive.com>
2018-01-05 09:42:40 +0800

    Ticket Title: Activity Logs 1. Created new migration script 2. Updated admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,39 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Dashboard</h1>
+<h3>Dashboard</h3>
+<hr>   
+<div class="container">
+    <div class="row">
+        <div class="col-*-*"></div>
+        <div class="col-*-*"></div>
+    </div>
+    <div class="row">
+        <h5>Admin Activity</h5>
+        <div class="col-lg-12">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Date</th> 
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Activity</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>John</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
 @endsection
 
 @section('script')
