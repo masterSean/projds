44c0167cda5206eee4c226db8463c25933c5422c
Sally Jean Sumeguin <sally.sumeguin@intechsive.com>
2018-02-21 00:04:16 +0800

    News

--- resources/views/client/frontline_services.blade.php
+++ resources/views/client/frontline_services.blade.php
@@ -4,10 +4,12 @@
     <div class="container">
         <div class="row" id="programs">
             @foreach ($data as $key => $value) 
-            <div class="col-md-4 grid-view">
+            <div class="col-md-3 grid-view">
                 <img src="{!! asset('/storage/programs/' . $value->image) !!}" id="program_icon"><br>
-            <h3 style="text-align:center;">{!! $value->title !!}</h3>
-                <p>{!! $value->description !!}</p>
+                <h3 style="text-align:center;">{!! $value->title !!}</h3>
+                <div style="text-overflow:ellipsis; overflow:hidden; white-space:nowrap; width:250px; ">
+                    <p>{!! $value->description !!}</p>
+                </div>
             </div>
             @endforeach
         </div>
